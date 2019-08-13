d1c417be99dc79632704d9b52835b4123dcd10a5
thisishale <you@example.com>
2019-08-13 11:26:19 +0430

    convert test data to 16khz

--- create_data/convert_to_16000_test.m
+++ create_data/convert_to_16000_test.m
@@ -1,32 +1,31 @@
 
 orig_path='D:\studies\university\thesis\speech_separation_codes\du16\donesomestuff';
-raw_path = strcat(orig_path,'\sec_mixed2_6_test\');
+raw_path = strcat(orig_path,'\sec_mixed2_6_test_all\');
 new_path = strcat(orig_path,'\test_16000\');
-files = dir(raw_path)';
-sorted_names=natsort({files.name});
+test_data = dir(raw_path)';
+test_data=natsort({test_data.name});
 i=0;
-for file1=3:length(sorted_names)
-    files2=dir(strcat(raw_path,sorted_names{file1}));
-    sorted_names2=natsort({files2.name});
-    if ~exist([strcat(new_path,sorted_names{file1})],'dir')
-        mkdir(new_path,sorted_names{file1})
+for test_folders=3:length(test_data)
+    test_files=dir(strcat(raw_path,test_data{test_folders}));
+    test_files_names=natsort({test_files.name});
+    if ~exist([strcat(new_path,test_data{test_folders})],'dir')
+        mkdir(new_path,test_data{test_folders})
     end
-    direct=strcat(new_path,sorted_names{file1});
-    for file2=3:length(sorted_names2)      
-            [y,Fs]=audioread(strcat(raw_path,sorted_names{file1},'\',sorted_names2{file2}));
-            [P,Q] = rat(16e3/Fs);
-            abs(P/Q*Fs-16000)
-            ynew = resample(y,P,Q);
-            %ynew=ynew/max(abs(ynew));
-            max_amp = max(cat(1,abs(ynew)));
-            scaling = 1/max_amp;
-            ynew = scaling * ynew;
-            audiowrite(strcat(direct,'\',sorted_names2{file2}),ynew,16000);
-%             [y,Fs]=audioread(strcat(raw_path,'\',sorted_names{file1},'\',sorted_names2{file2},'\',sorted_names3{file3}));
+    for names=3:length(test_files_names)      
+        [y,Fs]=audioread(strcat(raw_path,test_data{test_folders},'\',test_files_names{names}));
+        [P,Q] = rat(16e3/Fs);
+        abs(P/Q*Fs-16000)
+        ynew = resample(y,P,Q);
+        %ynew=ynew/max(abs(ynew));
+        max_amp = max(cat(1,abs(ynew)));
+        scaling = 1/max_amp;
+        ynew = scaling * ynew;
+        audiowrite(strcat(strcat(new_path,test_data{test_folders}),'\',test_files_names{names}),ynew,16000);
+%             [y,Fs]=audioread(strcat(raw_path,'\',sorted_names{test_folders},'\',test_files_names{names},'\',sorted_names3{file3}));
 %             audiowrite(strcat(direct,'\',sorted_names3{file3}),y,16000);
 %             clear y Fs
-        end
     end
+end
 % [y,Fs]=audioread(strcat(orig_path,'\mixed501_1.wav'));
 % audiowrite('mixed501_1_16bit.wav',y,16000);
 % clear y Fs
