5b7e305fe22b18ccbbbcd98c58af43a4b1d38900
thisishale <you@example.com>
2019-08-13 11:26:02 +0430

    convert dataset to 16 khz

---
+++ create_data/convert_to_16000_grid.m
@@ -0,0 +1,38 @@
+
+orig_path='D:\studies\university\thesis\speech_separation_codes\du16\donesomestuff';
+data_file='GRID';
+new_data='GRID_16';
+raw_path = strcat(orig_path,'\',data_file);
+new_path = strcat(orig_path,'\',new_data);
+files = dir(raw_path)';
+sorted_names=natsort({files.name});
+i=0;
+for file1=3:length(sorted_names)%1...34
+    files2=dir(strcat(raw_path,'\',sorted_names{file1}));
+    sorted_names2=natsort({files2.name});
+    if ~exist([strcat(new_path,'\',sorted_names{file1})],'dir')
+        mkdir(strcat(new_path,'\',sorted_names{file1}))
+    end
+    for file2=3:length(sorted_names2)%one
+        files3=dir(strcat(raw_path,'\',sorted_names{file1},'\',sorted_names2{file2}));
+        sorted_names3=natsort({files3.name});
+        for file3=3:length(sorted_names3)
+            [y,Fs]=audioread(strcat(raw_path,'\',sorted_names{file1},'\',sorted_names2{file2},'\',sorted_names3{file3}));
+            [P,Q] = rat(16e3/Fs);
+            abs(P/Q*Fs-16000)
+            ynew = resample(y,P,Q);
+            audiowrite(strcat(new_path,'\',sorted_names2{file2},'\',sorted_names3{file3}),ynew,16000);
+            clear y Fs
+        end
+    end
+end
+% [y,Fs]=audioread(strcat(orig_path,'\mixed501_1.wav'));
+% audiowrite('mixed501_1_16bit.wav',y,16000);
+% clear y Fs
+%asl_P56('down_mixed501_1_16bit.wav',16000,16)
+% [clean, srate]= audioread('down_mixed501_1_16bit.wav'); 
+% [Px, asl, c0]= asl_P56 ( clean, 16000, 32); 
+% addnoise('down_mixed501_1.wav', 16000, 16, 'down_mixed501_1.wav', 16000, 16, 0)
+
+
+
